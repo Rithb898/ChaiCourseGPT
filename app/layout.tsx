8a236ab0691cbca1fa7b835679100cb09a81d3da
Rithb898 <rithb2004@gmail.com>
2025-08-25 15:07:43 +0000

    add vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -39,8 +40,9 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
+          >
           {children}
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
